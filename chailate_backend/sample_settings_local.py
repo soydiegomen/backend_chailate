c1372f57fce1ebf4356db6d86ae4241961cd366c
Diego Mendoza <dmendoza@inflexionsoftware.com>
2016-08-03 23:28:32 -0500

    Se agrego un archivo de ejemplo para la configuración de la aplicación en distintos ambientes

---
+++ chailate_backend/sample_settings_local.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chailatebackend',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': '127.0.0.1',
+        'PORT': '8889',
+    }
+}
+MAILGUN_API='key'
